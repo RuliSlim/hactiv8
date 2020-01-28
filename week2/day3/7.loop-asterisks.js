66fcf64203376fd6ea783950444b98ce62010516
RuliSlim <ruli0to8@gmail.com>
2020-01-28 17:25:30 +0700

    change var to let

--- week2/day3/7.loop-asterisks.js
+++ week2/day3/7.loop-asterisks.js
@@ -10,7 +10,7 @@ console.log('No 2')
 let rows2 = 5
 for (let i = 1; i <= rows2; i++) {
   let bintang = '*'
-  for (var j = 1; j < rows2; j++) {
+  for (let j = 1; j < rows2; j++) {
     bintang = '*' + bintang
   }
   console.log(`${bintang}`)
