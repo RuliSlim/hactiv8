fc88f0ba299b5f5489f3d7271bdc335866a1fd20
RuliSlim <ruli0to8@gmail.com>
2020-02-01 12:29:26 +0700

    fix pasanganAngka on day3, cariMean and perkalianUnik done on day4

--- week3/day3/pasanganAngkaTerbesar.js
+++ week3/day3/pasanganAngkaTerbesar.js
@@ -1,23 +1,28 @@
-// Problem
-// Diberikan sebuah function pasanganTerbesar(angka) yang menerima 1 parameter berupa angka. Functiona akan menentukan pasangan dua digit angka mana yang paling besar dan me-return angka tersebut. Contoh, jika angka adalah 183928, maka function akan me-return 92, pasangan dua digit angka yang paling besar diantara yang lainnya.
-
-// Code
-// function pasanganTerbesar(num) {
-//   // you can only write your code here!
+// const pasanganTerbesar = num => {
+//   let besar = num.toString().split('')
+//   let odd = new Array(besar.length/2), 
+//   even = []
+//   if (besar.length % 2 == 0){
+//     for (let i = 0; i < besar.length/2; i++){
+//       let j=i
+//       j += i
+//       i%2==0 ? odd[i] = besar[j] + besar[j+1] : odd[i] = besar[j] + besar[j+1]
+//     }
+//     return odd.sort((a, b) => b - a)[0]
+//   } else return 'Input number invalid'
 // }
 
 const pasanganTerbesar = num => {
-  let besar = num.toString().split('')
-  let odd = new Array(besar.length/2), 
-  even = []
-  if (besar.length % 2 == 0){
-    for (let i = 0; i < besar.length/2; i++){
-      let j=i
-      j += i
-      i%2==0 ? odd[i] = besar[j] + besar[j+1] : odd[i] = besar[j] + besar[j+1]
+  let indexNum = new Number,
+      numArray = num.toString().split(''),
+      highestNum = num.toString().split('').sort((a,b) => b - a)[0]
+  for (let i = 0; i < numArray.length; i++) {
+    if (numArray[i]==highestNum) {
+      indexNum = i
+      break
     }
-    return odd.sort((a, b) => b - a)[0]
-  } else return 'Input number invalid'
+  }
+  return highestNum + numArray[indexNum+1]
 }
 
 // TEST CASES
